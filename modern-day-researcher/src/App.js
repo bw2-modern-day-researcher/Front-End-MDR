e4ae93e2db018475f7d865cb8702b3f2f1baf8a7
sriley <sarahriley.dev@gmail.com>
2019-04-25 16:25:23 -0400

    added icon and color

--- modern-day-researcher/src/App.js
+++ modern-day-researcher/src/App.js
@@ -1,46 +1,43 @@
-import React, { Component } from 'react';
-import './App.css';
-import { NavLink, Switch, Route, withRouter } from 'react-router-dom'
-import { Redirect } from 'react-router';
-import Register from './Authentication/Register'
-import Content from './components/Content/Content'
+import React, { Component } from "react";
+import "./App.css";
+import { NavLink, Switch, Route, withRouter } from "react-router-dom";
+import { Redirect } from "react-router";
+import Register from "./Authentication/Register";
+import Content from "./components/Content/Content";
 import Login from "./Authentication/Login";
-import Profile from './Authentication/Profile';
-
+import Profile from "./Authentication/Profile";
+import logo from "./assets/logo.jpg";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav className='top-nav'>
-          <NavLink to='/login' style={{ textDecoration: 'none' }}>Log in</NavLink>
+        <nav className="top-nav">
+          <NavLink to="/login">Log in</NavLink>
           &nbsp;|&nbsp;
-          <NavLink to='/register' style={{ textDecoration: 'none' }}>Register</NavLink>
+          <NavLink to="/register">Register</NavLink>
           &nbsp;|&nbsp;
-          <NavLink to='/profile' style={{ textDecoration: 'none' }}>My Profile</NavLink>
-          <NavLink to='/content' style={{ textDecoration: 'none' }}>
-          <h1>Research Pal</h1>
+          <NavLink to="/profile">My Profile</NavLink>
+          <NavLink to="/content">
+            <img src={logo} alt={logo} />
           </NavLink>
-          <br />          
+          <br />
         </nav>
 
         <section>
           <Switch>
-          <Route path='/login' component={Login} />
-            <Route path='/register' component={Register} />
-            <Route path='/profile' component={Profile} />
-            <Route path='/content' component={Content} />
-            <Route exact path='/' render={() => (
-              <Redirect to='/content' />
-            )
-            } />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/content" component={Content} />
+            <Route exact path="/" render={() => <Redirect to="/content" />} />
           </Switch>
         </section>
       </div>
     );
   }
   Logout = () => {
-    localStorage.removeItem('jwt');
+    localStorage.removeItem("jwt");
   };
 }
 
